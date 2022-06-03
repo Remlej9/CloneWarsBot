a1183fcd3c91a5f21eb8a39872fcaa6e5007eab9
Remlej9 <Remlej9@gmail.com>
2022-06-03 09:20:45 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -290,7 +290,7 @@ client.on('message', (message) => {
 });
 */
 
-const jobb = schedule.scheduleJob('00 00 07 20 * *', function(){
+const jobb = schedule.scheduleJob('30 22 07 * * *', function(){
 	count = Math.floor(Math.random() * 129);
 	client.channels.cache.get('910244563703193621').send(quotes[count])
 	.then(message => console.log(`Sent quote`))
